871f23bb562be26554c49ff709f908f94c93f39b
Stephen Waithumbi <stephenwaithumbi14@gmail.com>
2024-10-12 17:41:17 +0300

    Filter is so exiting

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(driversList, driverName){
+    return driversList.filter(driver => driver.toLowerCase() === driverName.toLowerCase());
+}
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(driver => driver.startsWith(name))
+}
+
+function matchName(drivers, name){
+    return drivers.filter(driver => driver.name === name)
+}
